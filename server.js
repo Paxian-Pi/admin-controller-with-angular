9496ecee108a49635c125096cc8646197916afff
Pius <ppax1706@gmail.com>
2021-05-07 23:09:07 +0100

    inital commit

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/admin-controller-with-angular'));
+app.use(express.static(__dirname + '/dist/fuse'));
 
 app.get('/*', function(req,res) {
     
-    res.sendFile(path.join(__dirname +'/dist/admin-controller-with-angular/index.html'));
+    res.sendFile(path.join(__dirname +'/dist/fuse/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
